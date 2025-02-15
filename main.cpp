924e38220cd3fd5d983ce2031b3de2a00be9504f
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-02-15 10:36:03 +0200

    created clsBankClient & added findByAccountNumber func

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
+#include "src/headerFiles/bankClient/clsBankClient.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
 
-  cout << "Hello World!";
+  clsBankClient client1 = clsBankClient::findClientByAccountNumber("A101");
+  client1.printClient();
+
+  clsBankClient client2 =
+      clsBankClient::findClientByAccountNumberAndPinCode("A101", 4151);
+  client2.printClient();
 
   return 0;
 }
