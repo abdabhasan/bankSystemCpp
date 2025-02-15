8e7b4203116af2057676f33dbc7d7891763fceb1
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-02-15 13:36:48 +0200

    created updateClient func

--- main.cpp
+++ main.cpp
@@ -3,14 +3,53 @@
 
 using namespace std;
 
+void readClientInfo(clsBankClient &client) {
+
+  cout << "\nEnter First Name: ";
+  client.setFirstName(clsMyInputValidateLib::readString());
+
+  cout << "\nEnter Last Name: ";
+  client.setLastName(clsMyInputValidateLib::readString());
+
+  cout << "\nEnter Email  : ";
+  client.setEmail(clsMyInputValidateLib::readString());
+
+  cout << "\nEnter phone : ";
+  client.setPhone(clsMyInputValidateLib::readString());
+
+  cout << "\nEnter pin code : ";
+  client.setPinCode(clsMyInputValidateLib::readIntNumber());
+
+  cout << "\nEnter account balance : ";
+  client.setAccountBalance(clsMyInputValidateLib::readDblNumber());
+}
+
+void updateClient() {
+  string accountNumber = "";
+
+  cout << "\nPlease enter client Account Number :  ";
+  accountNumber = clsMyInputValidateLib::readString();
+
+  while (!clsBankClient::isClientExist(accountNumber)) {
+    cout << "\nAccount not found, try again! ";
+    accountNumber = clsMyInputValidateLib::readString();
+  }
+
+  clsBankClient client =
+      clsBankClient::findClientByAccountNumber(accountNumber);
+  client.printClient();
+
+  cout << "\n\nUpdate Client info : ";
+  cout << "\n-----------------------------\n";
+
+  readClientInfo(client);
+
+  client.saveClient();
+}
+
 int main() {
 
-  clsBankClient client1 = clsBankClient::findClientByAccountNumber("A101");
-  client1.printClient();
-
-  clsBankClient client2 =
-      clsBankClient::findClientByAccountNumberAndPinCode("A101", 4151);
-  client2.printClient();
+  updateClient();
 
   return 0;
 }
